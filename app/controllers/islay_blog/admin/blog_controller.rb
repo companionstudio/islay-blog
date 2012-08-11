e818c31b699d4585d0b1e3035b959d2f1d1c7979
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-08-11 17:03:31 +0930

    Stub out blog overview.

--- app/controllers/islay_blog/admin/blog_controller.rb
+++ app/controllers/islay_blog/admin/blog_controller.rb
@@ -1,4 +1,12 @@
 class IslayBlog::Admin::BlogController < IslayBlog::Admin::ApplicationController
   header 'Blog'
   nav 'nav'
+
+  def index
+    # Pending entries
+    # Comments
+    # Tag cloud
+
+    @blog_tags = BlogTag.order('name')
+  end
 end
